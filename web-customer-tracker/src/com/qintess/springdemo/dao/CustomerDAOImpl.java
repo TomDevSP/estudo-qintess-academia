6609ea1e7dc90d2978334a860275fc6eeae25fa4
Tom <wellington.macedo89@outlook.com>
2020-04-24 16:55:45 -0300

    Formulário
    Adicionado métodos para modificar dados do formulário

--- web-customer-tracker/src/com/qintess/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/qintess/springdemo/dao/CustomerDAOImpl.java
@@ -22,14 +22,51 @@ public class CustomerDAOImpl implements CustomerDAO {
 		// pegar a session atual do hibernate
 		Session currentSession = sessionFactory.getCurrentSession();
 		
-		// criar uma query
-		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		// criar uma query ordenada pelo last name
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
 		
 		// executar query e pegar result list
 		List<Customer> customers = theQuery.getResultList();
 		
 		// retornar os results
 		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		
+		// obter a sessão atual do hibernate
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// salvar ou atualizar o customer
+		currentSession.saveOrUpdate(theCustomer);
+		
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+		
+		// obter a sessão atual do hibernate
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// recupero/leio do bd usando a primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+		
+		// obter a sessão atual do hibernate
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// deletar o objeto com a primary key
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		
+		theQuery.setParameter("customerId", theId);
+		
+		theQuery.executeUpdate();		
 	}
 
 }
