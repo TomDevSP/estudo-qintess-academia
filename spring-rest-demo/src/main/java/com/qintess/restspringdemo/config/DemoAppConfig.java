bce26e8412b64dfaf03d15ba624913db5c5be193
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2020-05-07 19:16:39 -0300

    war + rest + spring

---
+++ spring-rest-demo/src/main/java/com/qintess/restspringdemo/config/DemoAppConfig.java
@@ -0,0 +1,14 @@
+package com.qintess.restspringdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.qintess.restspringdemo")
+public class DemoAppConfig {
+	
+	
+
+}
