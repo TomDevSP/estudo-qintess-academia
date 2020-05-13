33a11293474ad30fa8a00cc683b3df6a800ea68e
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2020-05-13 20:55:51 -0300

    refatorando

--- exercicioREST/src/main/java/com/qintess/exercicio/rest/StudentRestController.java
+++ exercicioREST/src/main/java/com/qintess/exercicio/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,9 +48,49 @@ public class StudentRestController {
 		
 		// apenas indexar da lista
 		
-		return theStudents.get(studentId);
+		// checar o studentId contra o tamanho da lista
 		
+		if (studentId >= theStudents.size() || (studentId < 0)) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+	
+		return theStudents.get(studentId);		
 	}
 	
+	// adc um controlador de exceção usando @ExceptionHandler
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+		
+		// criar um StudentErrorResponse
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		// retorna ResponseEntity
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+	// adc outro controlador de exceção, para capturar qualquer exceção
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+		
+		// criar um StudentErrorResponse
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+				
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+				
+		// retorna ResponseEntity
+				
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
 
 }
