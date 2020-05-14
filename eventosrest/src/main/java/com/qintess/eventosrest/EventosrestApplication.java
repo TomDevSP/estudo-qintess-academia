a4f435e25f6ac062344e24973c89fce030aad047
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2020-05-14 17:10:58 -0300

    Criando projeto APIRest
    Modificando o projeto evento para API

---
+++ eventosrest/src/main/java/com/qintess/eventosrest/EventosrestApplication.java
@@ -0,0 +1,13 @@
+package com.qintess.eventosrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosrestApplication.class, args);
+	}
+
+}
---
+++ eventosrest/src/test/java/com/qintess/eventosrest/EventosrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qintess.eventosrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
