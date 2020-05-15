164ba3d226b2a462bd624a365b44fc62a8a111fa
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2020-05-15 16:29:55 -0300

    update

--- 23-spring-data-jpa-cruddemo/src/main/java/com/qintess/springboot/cruddemo/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qintess.springboot.cruddemo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qintess.springboot.cruddemo.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
